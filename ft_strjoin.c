9196ff7273bb7562971cbb3878a7f3075abad41b
Alric Le Conte <ale-cont@student.42lyon.fr>
2022-11-11 14:03:25 +0100

    add ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,10 +6,23 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 14:41:32 by ale-cont          #+#    #+#             */
-/*   Updated: 2022/11/11 13:34:31 by ale-cont         ###   ########.fr       */
+/*   Updated: 2022/11/11 14:03:08 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	while (s1)
+		*str++ = *s1;
+	while (s2)
+		*str++ = *s2;
+	*str = '\0';
+	return (str);
+}
