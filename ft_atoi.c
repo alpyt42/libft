c2fd4cb5131da2a394f94c84b0b14d70e06f2b7b
Alric42Mac <ale-cont@student.42lyon.fr>
2023-03-30 12:09:30 +0200

    up

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 21:18:11 by ale-cont          #+#    #+#             */
-/*   Updated: 2022/11/21 12:01:24 by ale-cont         ###   ########.fr       */
+/*   Updated: 2023/03/28 11:45:37 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,88 @@
 
 int	ft_atoi(const char *str)
 {
-	size_t					i;
-	int						sign;
-	unsigned long long int	nb;
+	size_t	i;
+	int		sign;
+	long	nb;
 
 	i = 0;
 	sign = 1;
 	nb = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
+	if (!str)
+		return (0);
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
 			sign *= -1;
 		i++;
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (str[i])
+	{
+		if ((str[i] < '0' || str[i] > '9'))
+			return (0);
 		nb = nb * 10 + str[i++] - 48;
-	if (nb > LLONG_MAX && sign < 0)
+	}
+	if (nb == 0 && str[0] != '0' && str[1] == '\0')
 		return (0);
-	if (nb > LLONG_MAX && sign > 0)
+	return (sign * nb);
+}
+
+static int	after_space(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	if (str[0] && str[0] == ' ')
+		while (str[i++])
+			if (str[i] && str[i] != ' ')
+				return (1);
+	return (0);
+}
+
+static size_t	check_sign(char **str, unsigned long long *sign)
+{
+	size_t	i;
+
+	i = 0;
+	while (*str[i] && *str[i] == ' ')
+		i++;
+	if (*str[i] == '-' || *str[i] == '+')
+	{
+		if (*str[i] == '-')
+			*sign *= -1;
+		else
+			*str[i] = '0';
+		i++;
+	}
+	return (i);
+}
+
+long	ft_atoi_long(char *str)
+{
+	size_t				i;
+	unsigned long long	nb;
+	unsigned long long	sign;
+
+	nb = 0;
+	sign = 1;
+	if (!str)
+		return (-1);
+	i = check_sign(&str, &sign);
+	while (str[i])
+	{
+		if ((str[i] < '0' || str[i] > '9') && str[i] != ' ')
+			return (-1);
+		if (str[i] == ' ' && after_space(&str[i]))
+			return (-1);
+		else if (str[i] == ' ' && !after_space(&str[i]))
+			break ;
+		nb = nb * 10 + str[i++] - 48;
+	}
+	if (sign * nb == (unsigned long long)-1)
+		return (255);
+	if (nb > 9223372036854775807)
 		return (-1);
 	return (sign * nb);
 }
